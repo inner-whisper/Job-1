962691a95cb24896c001e947a72c3b86350dfb97
veskakoff <veskakov@gmail.com>
2011-10-04 22:40:34 +0400

    User sighup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,23 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-	    @title = @user.name
+		@title = @user.name
 	end
 
 	def new
 		@title = "Sign up"
+		@user = User.new
+	end
+	
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to @user
+			flash[:success] = "Welcome to the Sample App!"
+		else
+		@title = "Sign up"
+	
+		render 'new'
+		end
 	end
 end
